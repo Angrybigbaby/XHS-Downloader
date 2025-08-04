11e40c31d9a62f1f46a6032a3f584a4fb3e94116
Angrybigbaby <425108497@qq.com>
2025-08-04 15:22:53 +0800

    去掉作者id

--- source/application/app.py
+++ source/application/app.py
@@ -184,9 +184,7 @@ class XHS:
                     u,
                     container["动图地址"],
                     index,
-                    container["作者ID"]
-                    + "_"
-                    + self.CLEANER.filter_name(container["作者昵称"]),
+                    self.CLEANER.filter_name(container["作者昵称"]),
                     name,
                     container["作品类型"],
                     container["时间戳"],
